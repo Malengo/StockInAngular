172c3a5b0c9df6b63ea816a677ef4a010a9e3375
Malengo <jose.malengo@hotmail.com>
2022-01-23 16:06:14 -0300

    Create method filter

--- src/app/list-product/list-product.component.ts
+++ src/app/list-product/list-product.component.ts
@@ -9,7 +9,9 @@ import { Product } from '../repository/product/product.model';
 })
 export class ListProductComponent implements OnInit {
   products!: Product[];
-
+  filterDescription: string = '';
+  showPane: boolean = false;
+  productsOfFilter!: Product[];
   constructor(private productService: ProductServiceService) {}
 
   ngOnInit(): void {
@@ -18,5 +20,30 @@ export class ListProductComponent implements OnInit {
 
   getAllProduct(): Product[] {
     return this.productService.getAllProduct();
+  }
+
+  filterByDescription(event: KeyboardEvent): any {
+    this.productsOfFilter = [];
+    this.filterDescription = (<HTMLInputElement>event.target).value;
+    if (this.filterDescription == '') {
+      this.showPane = false;
+    } else {
+      this.showPane = true;
+      this.products.forEach((position) => {
+        if (position.description?.includes(this.filterDescription)) {
+          if (!this.productsOfFilter.length) {
+            this.productsOfFilter.push(position);
+          } else {
+            for (let x = 0; this.productsOfFilter.length; x++) {
+              if (position.idProduct == this.productsOfFilter[x].idProduct) {
+              }
+              this.productsOfFilter.push(position);
+              break;
+            }
+          }
+        } else {
+        }
+      });
+    }
   }
 }
